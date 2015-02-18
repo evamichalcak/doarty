4e251058735eb2059c5da62597c101337e6d107e
Eva Michalcakova <eva.michalcak@gmail.com>
2015-02-18 20:32:10 +0100

    my first controller

---
+++ app/controllers/scraper/scraper.server.controller.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var _ = require('lodash');
+// var express = require('express');
+// var fs = require('fs');
+// var request = require('request');
+// var cheerio = require('cheerio');
+
+
+/**
+ * Update user details
+ */
+exports.scrape = function(req, res) {
+	// Init Variables
+	// var user = req.user;
+	// var message = null;
+	console.log('scrapin\'...');
+};
+
+/**
+ * Send User
+ */
---
+++ app/routes/scrape.server.routes.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = function(app) {
+	// Root routing
+	var scraper = require('../../app/controllers/scraper/scraper.server.controller');
+	app.route('/scrape').get(scraper.scrape);
+	// var users = require('../../app/controllers/users.server.controller');
+	// app.route('/users/me2').get(users.me);
+
+	console.log('Here!');
+};
